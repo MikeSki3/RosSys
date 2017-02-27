449ebfca0b9290b13e2a43b143d26aa431789ab6
Mike-BatBerry <mikewski333@gmail.com>
2017-02-26 18:20:57 -0800

    removed files

--- build/foobar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/catkin_ws/src/foobar
-# Build directory: /home/mike/catkin_ws/build/foobar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
